756a1cb098b1ebf9e40b814278c6903e97a1387e
dorin.cazan <dorin.cazan@e-uvt.ro>
2025-09-01 11:18:50 +0300

    load in minio

--- public/index.js
+++ public/index.js
@@ -8,26 +8,15 @@ form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const file = fileInput.files[0];
-    if (!file) {
-        statusElem.textContent = 'Te rog selectează un fișier.';
-        return;
-    }
 
     statusElem.style.color = 'black';
 
     try {
-        console.log('Cer URL-ul pre-semnat de la backend...');
         const response = await fetch(`${GENERATE_URL_ENDPOINT}?fileName=${encodeURIComponent(file.name)}`);
         
-        if (!response.ok) {
-            throw new Error('Nu am putut obține URL-ul de upload de la server.');
-        }
-
         const { uploadUrl } = await response.json();
-        console.log('Am primit URL-ul de upload.');
 
         statusElem.textContent = 'Se încarcă fișierul...';
-        console.log('Încep upload-ul direct către MinIO...');
         
         const uploadResponse = await fetch(uploadUrl, {
             method: 'PUT',
